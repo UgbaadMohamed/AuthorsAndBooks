d07a0b64c8aee9ecc9a3507c566a95ae5a10975f
Ugbaad <ugbaad0012@gmail.com>
2022-11-03 14:36:24 +0100

    Done

--- src/Book.java
+++ src/Book.java
@@ -5,19 +5,15 @@ public class Book extends Author {
     private int pages;
     private Author author;
 
-
     public Author getAuthor() {
         return author;
     }
-
 
     public Book (String title, int pages, Author author, ArrayList<Book> list) {
         super();
         this.title = title;
         this.pages = pages;
         this.author = author;
-
-
     }
 
     public String getTitle() {
